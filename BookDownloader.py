f2d86469d751271c017ab9ebc649da257443b7bc
Declan <pixeledcube@gmail.com>
2021-09-13 15:55:48 +1000

    Outlined basic structure

---
+++ BookDownloader.py
@@ -0,0 +1,47 @@
+# -----------------------------------------------------#
+#             Archive.org Book downloader              #
+# -----------------------------------------------------#
+#
+#   Downloads books that you need to borrow from
+#   archive.org and stores the book pages in a folder.
+#   This project uses selenium to interface with
+#   archive.org
+#
+#   Usage:
+#       BookDownloader.py <username> <password> <ID>
+
+#   Notes:
+#       The script downloads the book images to the folder
+#       named <ID>. If the folder exists then the script
+#       will skip any files of the same name.
+
+#   Import the necessary libraries
+
+import os
+import sys
+
+# Check command line arguments
+
+username = ''   # archive.org username
+password = ''   # archive.org username
+archiveId = ''  # archive.org item ID
+
+# Open selenium window
+
+# Check to see if archive ID is valid
+
+# Check to see if the item is a book that can be borrowed
+
+# Create folder or check if folder exists
+
+# Start download loop
+
+# Go to Book page
+
+# Download page to folder (or skip if file exists)
+
+# end download loop
+
+# Close selenium window
+
+# print download stats
